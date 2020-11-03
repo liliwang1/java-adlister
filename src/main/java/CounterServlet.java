92a58de284111019d5e5bcfc6dbbb471e2d6cbea
liliwang <monsi0510@hotmail.com>
2020-11-03 10:06:20 -0600

    feat: add finished counterServlet class

---
+++ src/main/java/CounterServlet.java
@@ -0,0 +1,27 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "CounterServlet", urlPatterns = "/counter")
+public class CounterServlet extends HttpServlet {
+    int counter = 0;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        counter++;
+
+        String count = request.getParameter("counter");
+        if (count == null)
+            out.printf("<h3>Counter: %d</h3>%n", counter);
+        else {
+            counter = Integer.parseInt(count);
+            out.printf("<h3>Counter: %d</h3>%n", counter);
+        }
+    }
+}
